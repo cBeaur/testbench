6d907c62289bcf339f8f3486778ad765efa6e5c4
Cedrik Beauregard <cbeauregard@DESKTOP-J8HGU5V.>
2024-03-01 16:34:50 -0500

    Trying to output users when pressing button...

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+    return render_template('index.html')
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = {
+        0: "John Doe",
+        1: "Mark Hamill",
+        2: "Michael Jordan",
+    }
+    return render_template('index.html', users=users)
